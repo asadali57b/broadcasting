8ad62020d9f8221a74e1498f0f5d2da3cb24507d
asadali57b <asadali57b@gmail.com>
2025-05-16 11:10:48 +0500

    Final fix for Vercel deployment

--- api/index.js
+++ api/index.js
@@ -18,18 +18,34 @@
 // module.exports = app;
 // api/index.js
 // api/index.js
+// const express = require('express');
+// require('dotenv').config();
+// require('../database');
+// const broadcastingRoutes = require('../routes/broadcasting_routes');
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+
+// app.use('/api/broadcasting', broadcastingRoutes);
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
 const express = require('express');
-require('dotenv').config();
-require('../database');
-const broadcastingRoutes = require('../routes/broadcasting_routes');
+require('dotenv').config({ path: './.env' });
+require('../database'); // adjust path if needed
 
 const app = express();
+const serverless = require('serverless-http');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const broadcasting_routes = require('../routes/broadcasting_routes');
+app.use('/api/broadcasting', broadcasting_routes);
 
-app.use('/api/broadcasting', broadcastingRoutes);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = serverless(app);
